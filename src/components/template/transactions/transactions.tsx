a2b51583949aaec17a5e7a1f61193d2f49942a7f
ZweiTinte <zweitinte@gmail.com>
2023-03-13 20:01:07 +0100

    recurring by day in transaction overview

--- src/components/template/transactions/transactions.tsx
+++ src/components/template/transactions/transactions.tsx
@@ -12,31 +12,61 @@ const Transactions = ({
   accounts: Account[];
 }) => {
   return (
-    <>
-      <div className="gameLayout">
-        <div className="transactionsCard">
-          <div className="formRowDefault">
-            <Headline
-              text="Transactions Overview"
-              style="transactionsHeadline"
-            />
-            <Button
-              color={"addButton"}
-              onClick={() => navigate("/transactions/new")}
-              text={"Add New Transaction"}
-            />
-          </div>
-          <div className="transactionProps">
-            <span className="transactionId">ID</span>
-            <span className="transactionDate">Type</span>
-            <span className="transactionDate">Date</span>
-            <span className="transactionName">Name</span>
-            <span className="transactionCategory">Category</span>
-            <span className="transactionAmountHeadline">Amount</span>
-            <span className="transactionFrom">From</span>
-            <span className="transactionTo">To</span>
-          </div>
-          {transactions.map((item, i) => {
+    <div className="gameLayout">
+      <div className="transactionsCard">
+        <div className="formRowDefault">
+          <Headline text="Transactions Overview" style="transactionsHeadline" />
+          <Button
+            color={"addButton"}
+            onClick={() => navigate("/transactions/new")}
+            text={"Add New Transaction"}
+          />
+        </div>
+        <div className="transactionProps">
+          <span className="transactionId">ID</span>
+          <span className="transactionDate">Type</span>
+          <span className="transactionDate">Date</span>
+          <span className="transactionName">Name</span>
+          <span className="transactionCategory">Category</span>
+          <span className="transactionAmountHeadline">Amount</span>
+          <span className="transactionFrom">From</span>
+          <span className="transactionTo">To</span>
+        </div>
+        {transactions.map((item, i) => {
+          if (item.transactionType === "Recurring") {
+            if (
+              item.recurringPeriod === "Day" &&
+              item.recurringEnd &&
+              item.recurringGap
+            ) {
+              let recurringTransactions = [];
+              let recurringDate = new Date(item.date);
+              while (recurringDate < new Date(item.recurringEnd)) {
+                const itemCopy = structuredClone(item);
+                itemCopy.date = recurringDate.toISOString().split("T")[0];
+                recurringTransactions.push(itemCopy);
+                recurringDate.setDate(
+                  recurringDate.getDate() + item.recurringGap
+                );
+              }
+              return recurringTransactions.map((trans, j) => {
+                return (
+                  <div
+                    className={
+                      "transactionInfos" +
+                      (i === transactions.length - 1 &&
+                      j === recurringTransactions.length - 1
+                        ? ""
+                        : " dottedBorder")
+                    }
+                    key={j}
+                  >
+                    <TransactionItem transaction={trans} accounts={accounts} />
+                  </div>
+                );
+              });
+            }
+          } else {
             return (
               <div
                 className={
@@ -48,10 +78,10 @@ const Transactions = ({
                 <TransactionItem transaction={item} accounts={accounts} />
               </div>
             );
-          })}
-        </div>
+          }
+        })}
       </div>
-    </>
+    </div>
   );
 };
 
