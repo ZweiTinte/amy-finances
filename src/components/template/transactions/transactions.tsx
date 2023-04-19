5450a23e115891c788d571979bbccec649ed8295
ZweiTinte <zweitinte@gmail.com>
2023-04-19 21:51:43 +0200

    remove flickering from transaction page

--- src/components/template/transactions/transactions.tsx
+++ src/components/template/transactions/transactions.tsx
@@ -15,47 +15,54 @@ const Transactions = ({
   totalBalance: number;
 }) => {
   return (
-    <div className="gameLayout">
-      <div className="transactionsCard">
-        <div className="formRowDefault">
-          <Headline text="Transactions Overview" style="transactionsHeadline" />
-          <Button
-            color={"addButton"}
-            onClick={() => navigate("/transactions/new")}
-            text={"Add New Transaction"}
-          />
-        </div>
-        <div className="transactionProps">
-          <span className="transactionId">ID</span>
-          <span className="transactionDate">Type</span>
-          <span className="transactionDate">Date</span>
-          <span className="transactionName">Name</span>
-          <span className="transactionCategory">Category</span>
-          <span className="transactionAmountHeadline">Amount</span>
-          <span className="transactionFrom">From</span>
-          <span className="transactionTo">To</span>
-        </div>
-        {transactions.map((item, i) => {
-          return (
-            <div
-              className={
-                "transactionInfos" +
-                (i !== transactions.length - 1 ? " dottedBorder" : "")
-              }
-              key={i}
-            >
-              <TransactionItem transaction={item} accounts={accounts} />
+    <>
+      {transactions && accounts && totalBalance && (
+        <div className="gameLayout">
+          <div className="transactionsCard">
+            <div className="formRowDefault">
+              <Headline
+                text="Transactions Overview"
+                style="transactionsHeadline"
+              />
+              <Button
+                color={"addButton"}
+                onClick={() => navigate("/transactions/new")}
+                text={"Add New Transaction"}
+              />
             </div>
-          );
-        })}
-        <div className="accountsSummary">
-          <span className="transactionsSum">Total Balance:</span>
-          <span className="transactionAmount">
-            {euroFormat.format(totalBalance)}
-          </span>
+            <div className="transactionProps">
+              <span className="transactionId">ID</span>
+              <span className="transactionDate">Type</span>
+              <span className="transactionDate">Date</span>
+              <span className="transactionName">Name</span>
+              <span className="transactionCategory">Category</span>
+              <span className="transactionAmountHeadline">Amount</span>
+              <span className="transactionFrom">From</span>
+              <span className="transactionTo">To</span>
+            </div>
+            {transactions.map((item, i) => {
+              return (
+                <div
+                  className={
+                    "transactionInfos" +
+                    (i !== transactions.length - 1 ? " dottedBorder" : "")
+                  }
+                  key={i}
+                >
+                  <TransactionItem transaction={item} accounts={accounts} />
+                </div>
+              );
+            })}
+            <div className="accountsSummary">
+              <span className="transactionsSum">Total Balance:</span>
+              <span className="transactionAmount">
+                {euroFormat.format(totalBalance)}
+              </span>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
