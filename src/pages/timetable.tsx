6e4d87bd6961c714c26c685026dc1676cedb73c7
ZweiTinte <zweitinte@gmail.com>
2023-04-20 22:58:31 +0200

    file upload component

--- src/pages/timetable.tsx
+++ src/pages/timetable.tsx
@@ -1,11 +1,34 @@
 import * as React from "react";
 import Timetable from "../components/atoms/timetable";
+import FileUpload from "../components/atoms/fileUpload";
 
 const TestPage = () => {
+  const [image, setImage] = React.useState<File | null>(null);
+  const [imageUrl, setImageUrl] = React.useState<string>("");
+
+  React.useEffect(() => {
+    if (image) {
+      setImageUrl(URL.createObjectURL(image));
+    }
+  }, [image]);
+
   return (
     <div className="gameLayout">
       <div className="transactionsCard">
         <Timetable />
+        <div className="formRow">
+          <FileUpload
+            id="upload"
+            onChange={(e) => {
+              setImage(e.target.files[0]);
+            }}
+            text="Upload"
+            color="uploadButton"
+          />
+          {imageUrl && (
+            <img id="blah" src={imageUrl} alt="your image" className="image" />
+          )}
+        </div>
       </div>
     </div>
   );
